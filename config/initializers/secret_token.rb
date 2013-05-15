f6073745a0860c96f5d8118dcf862b5f15659f06
Meris Williams <merisw@gmail.com>
2013-05-14 22:02:56 -0700

    Generate date tracker and added rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DateTracker::Application.config.secret_token = 'bb564aa662b0c6d033c450fb2eb9484d1e6a2dbae2607a2f23e9f9dfa42701a6c3a3c06b1f74dc6937f6af46dbdc004bc4639c2efa1079f247f22e75739096aa'
